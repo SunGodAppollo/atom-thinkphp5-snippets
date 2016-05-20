f87802211bd1cff2ed0327dbbeac9fb171753aec
chunice <hrb@usa.com>
2016-05-20 14:19:10 +0800

    清理无用文件

--- spec/thinkphp5-snippets-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import Thinkphp5SnippetsView from '../lib/thinkphp5-snippets-view';
-
-describe('Thinkphp5SnippetsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
